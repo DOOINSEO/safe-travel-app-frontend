233aa353e958baa690be6f3e71dcee4a408e90a3
leesu <a91364090@gmail.com>
2025-11-06 14:22:22 +0900

    Google Maps 지도 적용

--- web/src/pages/Map.jsx
+++ web/src/pages/Map.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import {GoogleMap, LoadScript} from '@react-google-maps/api';
 
-export default function Map() {
-  return <div>지도 페이지입니다.</div>;
+const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh',
+};
+
+// 캄보디아 프놈펜
+const center = {
+  lat: 11.5564,
+  lng: 104.9282,
+};
+
+export default function MAP() {
+  return (
+    <div className="w-full h-screen">
+      <LoadScript googleMapsApiKey={googleMapsApiKey}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12} options={{}}></GoogleMap>
+      </LoadScript>
+    </div>
+  );
 }
