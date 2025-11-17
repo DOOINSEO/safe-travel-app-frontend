ddcb4426212ac3f6e8251efe1f9d06c4fd28aaa8
Park Jae Hyeong <pixelarttia@gmail.com>
2025-11-17 13:39:08 +0900

    proxy 백엔드 서버 주소 추가http://localhost:8080

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,10 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-  server: {
-    historyApiFallback: true,
-    host: '0.0.0.0',
-    port: 5173,
-  },
+    plugins: [react(), tailwindcss(), svgr()],
+    server: {
+        historyApiFallback: true,
+        host: '0.0.0.0',
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080', // 백엔드 서버 주소
+                changeOrigin: true,
+            },
+        },
+    },
 });
